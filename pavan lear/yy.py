47399aa5e74418c27fe92cf3a32d7fbfd826eacf
saikonagandla <sai.konagandla@gmail.com>
2021-12-26 15:59:16 +0530

    ooooo

---
+++ pavan lear/yy.py
@@ -0,0 +1,4 @@
+list1= (9,7,8)
+l2=str(list1).split(',')
+l3=' '.join(l2)
+print(l3)
---
+++ rr.py
@@ -0,0 +1,11 @@
+str="This is a typical sentence"
+l=str.split()
+print(l)
+for i in l:
+    if len(l[i])>5:
+        y=l[::-1]
+    else:
+       y= print(l)
+
+z=''.join(l)
+print(y)
